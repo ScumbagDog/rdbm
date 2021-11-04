82eae4d8f7fb527e67c0f120e5953efb2a25c1fc
ScumbagDog <lbrack17@student.aau.dk>
2021-11-04 11:06:49 +0100

    Formatted dbm.rs

--- src/dbm.rs
+++ src/dbm.rs
@@ -5,11 +5,11 @@ pub mod dbm {
      * As such, it should be indexed with an offset.
      *
      */
-    
-    use std::ops::Add;
-use crate::dbm::dbm::ConstraintOp::{LessThanEqual, LessThan};
+
     use crate::bitvector::Bitvector;
+    use crate::dbm::dbm::ConstraintOp::{LessThan, LessThanEqual};
     use std::fmt;
+    use std::ops::Add;
     struct DBM<T, N> {
         matrix: Vec<T>,
         clock_names: Vec<N>,
@@ -28,52 +28,46 @@ use crate::dbm::dbm::ConstraintOp::{LessThanEqual, LessThan};
         LessThan,
     }
 
-     impl<T: std::fmt::Display> fmt::Display for Bound<T> {
-
+    impl<T: std::fmt::Display> fmt::Display for Bound<T> {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
             write!(f, "({}, {})", self.boundval, self.constraint_op)
         }
-     }
+    }
 
     impl From<bool> for ConstraintOp {
-
         fn from(val: bool) -> Self {
             match val {
                 true => LessThan,
-                false => LessThanEqual
+                false => LessThanEqual,
             }
         }
     }
 
     impl From<&bool> for ConstraintOp {
-
         fn from(val: &bool) -> Self {
             match *val {
                 true => LessThan,
-                false => LessThanEqual
+                false => LessThanEqual,
             }
         }
     }
 
     impl From<ConstraintOp> for bool {
-
         fn from(val: ConstraintOp) -> Self {
             match val {
                 LessThan => true,
-                LessThanEqual => false
+                LessThanEqual => false,
             }
         }
     }
 
     impl Default for ConstraintOp {
-
         fn default() -> Self {
             LessThanEqual
         }
     }
 
     impl Add for ConstraintOp {
-
         fn add(self, rhs: Self) -> <Self as std::ops::Add<Self>>::Output {
             (self.into() && rhs.into()).into() //cast to bool to perform and operator, then cast back once we are done
         }
@@ -81,11 +75,10 @@ use crate::dbm::dbm::ConstraintOp::{LessThanEqual, LessThan};
     }
 
     impl fmt::Display for ConstraintOp {
-
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
             match self {
-                LessThanEqual =>    write!(f, "≤"),
-                LessThan =>         write!(f, "<"),
+                LessThanEqual => write!(f, "≤"),
+                LessThan => write!(f, "<"),
             }
         }
     }
@@ -123,7 +116,7 @@ use crate::dbm::dbm::ConstraintOp::{LessThanEqual, LessThan};
             }
         }
 
-        fn set_element(&mut self, row: usize, col: usize, elem: T) -> Result<(), ()>{
+        fn set_element(&mut self, row: usize, col: usize, elem: T) -> Result<(), ()> {
             match self.get_dimsize() {
                 dimsize | dimsize if row > dimsize - 1 => Err(()),
                 dimsize | dimsize if col > dimsize - 1 => Err(()),
@@ -136,12 +129,12 @@ use crate::dbm::dbm::ConstraintOp::{LessThanEqual, LessThan};
         }
 
         fn get_bitval(&self, row: usize, col: usize) -> Option<bool> {
-            self.bitvec
-                .get_bit((row * self.get_dimsize() + col) as u32)
+            self.bitvec.get_bit((row * self.get_dimsize() + col) as u32)
         }
 
         fn set_bitval(&mut self, row: usize, col: usize, val: bool) -> Result<(), ()> {
-            self.bitvec.set_bit((row * self.get_dimsize() + col) as u32, val)
+            self.bitvec
+                .set_bit((row * self.get_dimsize() + col) as u32, val)
         }
 
         fn get_bound(&self, row: usize, col: usize) -> Option<Bound<T>> {
@@ -156,55 +149,57 @@ use crate::dbm::dbm::ConstraintOp::{LessThanEqual, LessThan};
                 }
             }
             return None;
-
         }
 
         fn set_bound(&mut self, row: usize, col: usize, bound: Bound<T>) -> Result<(), ()> {
             let set_element_status = self.set_element(row, col, bound.boundval);
             let set_bit_status = self.set_bitval(row, col, bound.constraint_op.into());
             match set_element_status {
-                Ok(_) => {
-                    match set_bit_status {
-                        Ok(_) => Ok(()),
-                        Err(_) => Err(()),
-                    }
-                }
+                Ok(_) => match set_bit_status {
+                    Ok(_) => Ok(()),
+                    Err(_) => Err(()),
+                },
                 Err(_) => Err(()),
             }
         }
 
-        fn get_bound_iter(&self) -> impl Iterator<Item = Bound<&T>> + '_ { //This function looks deceptively simple. It is an absolute mess behind the scenes.
-            let matrix_iter = self.matrix.iter();                          //If you ever decide to refactor it, prepare yourself for generic
+        fn get_bound_iter(&self) -> impl Iterator<Item = Bound<&T>> + '_ {
+            //This function looks deceptively simple. It is an absolute mess behind the scenes.
+            let matrix_iter = self.matrix.iter(); //If you ever decide to refactor it, prepare yourself for generic
             let bitvec_iter = self.bitvec.get_iterator();
-            matrix_iter.zip(bitvec_iter)
-                       .map(|(val, bitval)| Bound{boundval: val, constraint_op: ConstraintOp::from(bitval),})
+            matrix_iter.zip(bitvec_iter).map(|(val, bitval)| Bound {
+                boundval: val,
+                constraint_op: ConstraintOp::from(bitval),
+            })
         }
 
         fn get_index_of_clock(&self, clock: N) -> Option<usize> {
             self.clock_names.iter().position(|e| e == &clock)
         }
 
-        fn consistent(&self) -> bool { //should be run after the tighten function, as index (i, i) will be annotated there
-                                       //alternatively after another transformation, as it preserves the consistent status
+        fn consistent(&self) -> bool {
+            //should be run after the tighten function, as index (i, i) will be annotated there
+            //alternatively after another transformation, as it preserves the consistent status
             (0..self.get_dimsize())
-                .map(|c| self.get_bound(c, c).unwrap() == Default::default())
+                .map(|c| self.get_bound(c, c).unwrap() == Default::default()) //could be optimized to short circuit early, but this is easier to implement and looks nicer, so ¯\_(ツ)_/¯
                 .fold(true, |acc, e| acc && e)
         }
 
-        fn relation(first: &DBM<T, N>, second: &DBM<T, N>) -> bool { //I considered doing this as a method, but I ultimately decided to do it as a normal function, as it looks somewhat nicer
-            first.get_dimsize() == second.get_dimsize() && { //dimension check for short circuit and iterator size later.
+        fn relation(first: &DBM<T, N>, second: &DBM<T, N>) -> bool {
+            //I considered doing this as a method, but I ultimately decided to do it as a normal function, as it looks somewhat nicer
+            first.get_dimsize() == second.get_dimsize() && {
+                //dimension check for short circuit and iterator size later.
                 let first_iter = first.get_bound_iter();
                 let second_iter = second.get_bound_iter();
                 first_iter.le(second_iter) //check if every element in first_iter is less than or equal to its corresponding element in second_iter.
-                //Will return true even if first_iter has fewer elements than second_iter (so long as they are less than or equal to second_iter), so dimension check is still needed
+                                           //Will return true even if first_iter has fewer elements than second_iter (so long as they are less than or equal to second_iter), so dimension check is still needed
             }
         }
 
-        fn satisfied(row_clock: N, col_clock: N, op: ConstraintOp, constant: T) {
+        fn satisfied(row_clock: N, col_clock: N, op: ConstraintOp, constant: T) {}
 
-        }
-
-        fn tighten(&mut self){ //Essentially the Floyd-Warshall algorithm
+        fn close(&mut self) {
+            //Essentially the Floyd-Warshall algorithm
             let dimsize = self.get_dimsize();
             for k in 0..dimsize {
                 for i in 0..dimsize {
@@ -212,9 +207,9 @@ use crate::dbm::dbm::ConstraintOp::{LessThanEqual, LessThan};
                         let ij_bound = self.get_bound(i, j).unwrap();
                         let ik_bound = self.get_bound(i, k).unwrap();
                         let kj_bound = self.get_bound(k, j).unwrap();
-                        let ikj_bound = Bound
-                        {boundval: ik_bound.boundval + kj_bound.boundval,
-                         constraint_op: ik_bound.constraint_op + kj_bound.constraint_op,
+                        let ikj_bound = Bound {
+                            boundval: ik_bound.boundval + kj_bound.boundval,
+                            constraint_op: ik_bound.constraint_op + kj_bound.constraint_op,
                         };
                         if ij_bound > ikj_bound {
                             self.set_bound(i, j, ikj_bound).unwrap();
@@ -241,11 +236,14 @@ use crate::dbm::dbm::ConstraintOp::{LessThanEqual, LessThan};
     }
 
     impl<
-            T: std::default::Default + std::cmp::PartialOrd + std::fmt::Display + Clone + std::ops::Add<Output = T>,
+            T: std::default::Default
+                + std::cmp::PartialOrd
+                + std::fmt::Display
+                + Clone
+                + std::ops::Add<Output = T>,
             N: std::default::Default + std::cmp::Ord,
         > fmt::Display for DBM<T, N>
     {
-
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
             let mut bitstring = self.bitvec.get_bits_as_vec();
             let mut bitstring_chunks = bitstring.chunks(self.get_dimsize());
@@ -255,65 +253,104 @@ use crate::dbm::dbm::ConstraintOp::{LessThanEqual, LessThan};
                 let mut r = r.into_iter().peekable();
                 while let Some(e) = r.next() {
                     let e_bitval = bit_r.next().unwrap();
-                    let bound = Bound {boundval: e, constraint_op: ConstraintOp::from(*e_bitval)};
+                    let bound = Bound {
+                        boundval: e,
+                        constraint_op: ConstraintOp::from(*e_bitval),
+                    };
                     write!(f, "{}", bound);
                     match r.peek() {
                         Some(a) => write!(f, ", "),
                         None => write!(f, "|\n"),
-                    }.unwrap();
-                };
+                    }
+                    .unwrap();
+                }
             }
-        Ok(())
+            Ok(())
         }
     }
 
     #[test]
     fn bound_lte_test1() {
-        let smaller_bound = Bound { boundval: 0, constraint_op: ConstraintOp::LessThanEqual };
-        let greater_bound = Bound { boundval: 0, constraint_op: ConstraintOp::LessThanEqual };
+        let smaller_bound = Bound {
+            boundval: 0,
+            constraint_op: ConstraintOp::LessThanEqual,
+        };
+        let greater_bound = Bound {
+            boundval: 0,
+            constraint_op: ConstraintOp::LessThanEqual,
+        };
         assert_eq!(smaller_bound <= greater_bound, true);
     }
 
     #[test]
     fn bound_lte_test2() {
-        let smaller_bound = Bound { boundval: 0, constraint_op: ConstraintOp::LessThanEqual };
-        let greater_bound = Bound { boundval: 10, constraint_op: ConstraintOp::LessThanEqual };
+        let smaller_bound = Bound {
+            boundval: 0,
+            constraint_op: ConstraintOp::LessThanEqual,
+        };
+        let greater_bound = Bound {
+            boundval: 10,
+            constraint_op: ConstraintOp::LessThanEqual,
+        };
         assert_eq!(smaller_bound <= greater_bound, true);
         assert_eq!(greater_bound <= smaller_bound, false);
     }
 
     #[test]
     fn bound_lte_test3() {
-        let smaller_bound = Bound { boundval: 0, constraint_op: ConstraintOp::LessThanEqual };
-        let greater_bound = Bound { boundval: 0, constraint_op: ConstraintOp::LessThan };
+        let smaller_bound = Bound {
+            boundval: 0,
+            constraint_op: ConstraintOp::LessThanEqual,
+        };
+        let greater_bound = Bound {
+            boundval: 0,
+            constraint_op: ConstraintOp::LessThan,
+        };
         assert_eq!(smaller_bound <= greater_bound, true);
         assert_eq!(greater_bound <= smaller_bound, false);
     }
 
     #[test]
     fn bound_le_test1() {
-        let smaller_bound = Bound { boundval: 0, constraint_op: ConstraintOp::LessThanEqual };
-        let greater_bound = Bound { boundval: 0, constraint_op: ConstraintOp::LessThan };
+        let smaller_bound = Bound {
+            boundval: 0,
+            constraint_op: ConstraintOp::LessThanEqual,
+        };
+        let greater_bound = Bound {
+            boundval: 0,
+            constraint_op: ConstraintOp::LessThan,
+        };
         assert_eq!(smaller_bound < greater_bound, true);
         assert_eq!(greater_bound < smaller_bound, false);
     }
 
     #[test]
     fn bound_le_test2() {
-        let smaller_bound = Bound { boundval: 0, constraint_op: ConstraintOp::LessThanEqual };
-        let greater_bound = Bound { boundval: 0, constraint_op: ConstraintOp::LessThanEqual };
+        let smaller_bound = Bound {
+            boundval: 0,
+            constraint_op: ConstraintOp::LessThanEqual,
+        };
+        let greater_bound = Bound {
+            boundval: 0,
+            constraint_op: ConstraintOp::LessThanEqual,
+        };
         assert_eq!(smaller_bound < greater_bound, false);
         assert_eq!(greater_bound < smaller_bound, false);
     }
 
     #[test]
     fn bound_le_test3() {
-        let smaller_bound = Bound { boundval: 0, constraint_op: ConstraintOp::LessThanEqual };
-        let greater_bound = Bound { boundval: 10, constraint_op: ConstraintOp::LessThanEqual };
+        let smaller_bound = Bound {
+            boundval: 0,
+            constraint_op: ConstraintOp::LessThanEqual,
+        };
+        let greater_bound = Bound {
+            boundval: 10,
+            constraint_op: ConstraintOp::LessThanEqual,
+        };
         assert_eq!(smaller_bound < greater_bound, true);
         assert_eq!(greater_bound < smaller_bound, false);
     }
-
 
     #[test]
     fn dbm_index_test1() {
@@ -343,7 +380,10 @@ use crate::dbm::dbm::ConstraintOp::{LessThanEqual, LessThan};
     fn dbm_bit_consistency_test1() {
         let clocks = vec!["c1", "c2", "c3", "c4"];
         let dbm = DBM::<i32, &str>::new(clocks);
-        assert_eq!(dbm.get_dimsize() * dbm.get_dimsize(), dbm.bitvec.get_length() as usize);
+        assert_eq!(
+            dbm.get_dimsize() * dbm.get_dimsize(),
+            dbm.bitvec.get_length() as usize
+        );
     }
 
     #[test]
@@ -386,15 +426,20 @@ use crate::dbm::dbm::ConstraintOp::{LessThanEqual, LessThan};
         let mut elem = dbm.matrix.get_mut(1).unwrap();
         *elem = 4;
         let bound = dbm.get_bound(0, 1);
-        assert_eq!(bound.unwrap(), Bound{boundval: 4, constraint_op: ConstraintOp::LessThan});
+        assert_eq!(
+            bound.unwrap(),
+            Bound {
+                boundval: 4,
+                constraint_op: ConstraintOp::LessThan
+            }
+        );
     }
-
 
     #[test]
     fn dbm_consistency_test1() {
         let clocks = vec!["c1", "c2", "c3", "c4"];
         let mut dbm = DBM::<i32, &str>::new(clocks);
-        dbm.tighten();
+        dbm.close();
         assert_eq!(dbm.consistent(), true); //a dbm filled with (0, lte) should be consistent
     }
 
@@ -404,7 +449,7 @@ use crate::dbm::dbm::ConstraintOp::{LessThanEqual, LessThan};
         let mut dbm = DBM::<i32, &str>::new(clocks);
         let mut val = dbm.matrix.get_mut(1).unwrap(); //get mutable reference to the value in (0, 1)
         *val = 1; //set (0, 1) to be 1
-        dbm.tighten();
+        dbm.close();
         assert_eq!(dbm.consistent(), true); // as the upper bound in (0, 1) isn't smaller than the lower bound in (1, 0), dbm should be consistent
     }
 
@@ -415,7 +460,7 @@ use crate::dbm::dbm::ConstraintOp::{LessThanEqual, LessThan};
         let dimsize = dbm.get_dimsize();
         let mut val = dbm.matrix.get_mut(dimsize).unwrap(); //get mutable reference to the value in (1, 0). (we use dimsize to skip the first row)
         *val = 1; //set (1, 0) to be 1, meaning that 0-x <= 1 ~ -x <= 1
-        dbm.tighten();
+        dbm.close();
         assert_eq!(dbm.consistent(), true); // While this means that the zone is behind the x-axis which violates our implicit constraint that clocks are non-negative, it doesn't make it inconsistent, as we can tighten the bound.
     }
 
@@ -426,7 +471,7 @@ use crate::dbm::dbm::ConstraintOp::{LessThanEqual, LessThan};
         let dimsize = dbm.get_dimsize();
         let mut val = dbm.matrix.get_mut(dimsize).unwrap(); //get mutable reference to the value in (1, 0). (we use dimsize to skip the first row)
         *val = -1; //set (1, 0) to be -1, meaning that 0-x <= -1 ~ -x <= -1
-        dbm.tighten();
+        dbm.close();
         assert_eq!(dbm.consistent(), false); //As the corresponding bound in (0, 1) is (0 <=), this would mean that the intersection between the bounds is Ø (empty), as no x is both smaller than 0 and greater than 1/ !!Inconsistent
     }
 
@@ -436,7 +481,7 @@ use crate::dbm::dbm::ConstraintOp::{LessThanEqual, LessThan};
         let mut dbm = DBM::<i32, &str>::new(clocks);
         let mut val = dbm.matrix.get_mut(1).unwrap(); //get mutable reference to the value in (0, 1)
         *val = -1;
-        dbm.tighten();
+        dbm.close();
         assert_eq!(dbm.consistent(), false); // Same deal as in consistency test 4, the bounds are flipped. No x that is smaller than -1 and greater than 0 => inconsistent DBM
     }
 
@@ -487,7 +532,7 @@ use crate::dbm::dbm::ConstraintOp::{LessThanEqual, LessThan};
         assert_eq!(DBM::relation(&dbm_gte, &dbm_le), false); //but dimensions are different, so return false
     }
 
-     #[test]
+    #[test]
     fn dbm_relation_test5() {
         let clocks = vec!["c1", "c2", "c3", "c4"];
         let mut dbm_le = DBM::<i32, &str>::new(clocks.to_owned());
